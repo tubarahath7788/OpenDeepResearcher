fcd2202c6c9422f468e5b66948ff553ee160530a
tubarahath7788 <tubarahath7788@gmail.com>
2026-03-04 17:07:01 +0530

    Rename and enhance search function for better output
    Refactor search function to include search depth and format results.

--- searcher.py
+++ searcher.py
@@ -1,13 +1,18 @@
 from tavily import TavilyClient
 from config.settings import TAVILY_API_KEY
 
-
-def search_agent(query: str):
+def searcher_agent(query: str):
     client = TavilyClient(api_key=TAVILY_API_KEY)
 
-    results = client.search(
+    response = client.search(
         query=query,
+        search_depth="basic",
         max_results=3
     )
 
-    return results
+    results = []
+
+    for result in response["results"]:
+        results.append(f"Title: {result['title']}\nURL: {result['url']}\n")
+
+    return "\n".join(results)
