fb265dbce54c2c6e3be81b5c8a169c89bcea0408
tubarahath7788 <tubarahath7788@gmail.com>
2026-03-04 17:04:19 +0530

    Refactor planner_agent to include API key
    Updated the planner_agent function to include OPENAI_API_KEY and changed the import statement for ChatOpenAI.

--- planner.py
+++ planner.py
@@ -1,18 +1,19 @@
-from langchain.chat_models import ChatOpenAI
-from config.settings import LLM_BASE_URL, LLM_MODEL
-
+from langchain_openai import ChatOpenAI
+from config.settings import LLM_BASE_URL, LLM_MODEL, OPENAI_API_KEY
 
 def planner_agent(query: str):
     llm = ChatOpenAI(
         base_url=LLM_BASE_URL,
         model=LLM_MODEL,
+        api_key=OPENAI_API_KEY,
         temperature=0.3
     )
 
     prompt = f"""
-    Break this research topic into 3 clear sub-questions:
+    Break this research topic into 3 sub-questions:
     Topic: {query}
     """
 
     response = llm.invoke(prompt)
     return response.content
+
